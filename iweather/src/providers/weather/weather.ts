a2f4ce3614fe5869080f99ff01b9d76fc5b66df2
rahat <rahat.forja@gmail.com>
2018-02-14 16:31:34 -0600

    added wunderground api get

--- iweather/src/providers/weather/weather.ts
+++ iweather/src/providers/weather/weather.ts
@@ -1,11 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherProvider {
+  apiKey = '79348d8d03a7f427';
+  url;
 
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'
+      + this.apiKey + '/conditions/q/'; //CA/San_Francisco.json';
+  }
+
+  getWeather(city, state) {
+    return this.http.get(this.url + '/' + state + '/' + city + '.json')
+      .map(res => res.json());
   }
 
 }
