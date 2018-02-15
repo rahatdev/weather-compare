6fadf0d4b2512595f480ae49505f4218196aa7a4
rahat <rahat.forja@gmail.com>
2018-02-15 02:38:28 -0600

    injected weather api provider

--- iweather/src/pages/home/home.ts
+++ iweather/src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  weather: any;
+  location: {
+    city: string,
+    state: string
   }
 
+  constructor(
+    public navCtrl: NavController,
+    private _weather: WeatherProvider
+  ) { }
+
+  ionViewWillEnter() {
+    this.location = {
+      city: 'Miami',
+      state: 'FL'
+    }
+
+    this._weather.getWeather(this.location.city, this.location.state)
+      .subscribe(weather => {
+        this.weather = weather.current_observation;
+      })
+  }
 }
